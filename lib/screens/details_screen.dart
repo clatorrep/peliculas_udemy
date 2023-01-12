a1eaa7f3a1f378312296dc27b1f520f7ce6673dd
Cristobal Latorre Padilla <clatorre@bcnschool.cl>
2023-01-12 14:44:11 -0300

    ADD A APPBAR - add a CustomScrollView - add AppBar using sliver#

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,10 +5,43 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('DetailScreen'),
+    // TODO: Cambiar luego por una instancia de movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [_CustomAppBar()],
       ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+          titlePadding: const EdgeInsets.all(0),
+          title: Container(
+            width: double.infinity,
+            alignment: Alignment.bottomCenter,
+            color: Colors.black26,
+            child: const Text(
+              'movie.title',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          background: const FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage('https://via.placeholder.com/500x300'),
+            fit: BoxFit.cover,
+          )),
     );
   }
 }
